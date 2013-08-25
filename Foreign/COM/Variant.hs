4a15a52a6ef66f5ce330e5e258a4a2bf8a41150f
Tim Matthews <tim.matthews7@gmail.com>
2013-08-25 18:13:19 +1200

    Fixed bug and improved cos slightly

--- Foreign/COM/Variant.hs
+++ Foreign/COM/Variant.hs
@@ -11,6 +11,17 @@ module Foreign.COM.Variant (
   HVarType(..),
 -- * Var Type Definitions
 -- $varTypeDef
+  vt_EMPTY,
+  vt_NULL,
+  vt_I2,
+  vt_I4,
+  vt_R4,
+  vt_R8,
+  vt_CY,
+  vt_DATE,
+  vt_BSTR,
+  vt_DISPATCH,
+  vt_ERROR,
   vt_BOOL,
   vt_VARIANT,
   vt_UNKNOWN,
@@ -62,59 +73,6 @@ import Foreign.Storable
 
 data Variant = Variant VarType Word64 deriving (Show, Eq)
 
--- |
--- >vt_EMPTY            =      0 :: VarType
--- >vt_NULL             =      1 :: VarType
--- >vt_I2               =      2 :: VarType
--- >vt_I4               =      3 :: VarType
--- >vt_R4               =      4 :: VarType
--- >vt_R8               =      5 :: VarType
--- >vt_CY               =      6 :: VarType
--- >vt_DATE             =      7 :: VarType
--- >vt_BSTR             =      8 :: VarType
--- >vt_DISPATCH         =      9 :: VarType
--- >vt_ERROR            =     10 :: VarType
--- >vt_BOOL             =     11 :: VarType
--- >vt_VARIANT          =     12 :: VarType
--- >vt_UNKNOWN          =     13 :: VarType
--- >vt_DECIMAL          =     14 :: VarType
--- >vt_I1               =     16 :: VarType
--- >vt_UI1              =     18 :: VarType
--- >vt_UI2              =     18 :: VarType
--- >vt_UI4              =     19 :: VarType
--- >vt_I8               =     20 :: VarType
--- >vt_UI8              =     21 :: VarType
--- >vt_INT              =     22 :: VarType
--- >vt_UINT             =     23 :: VarType
--- >vt_VOID             =     24 :: VarType
--- >vt_HRESULT          =     25 :: VarType
--- >vt_PTR              =     26 :: VarType
--- >vt_SAFEARRAY        =     27 :: VarType
--- >vt_CARRAY           =     28 :: VarType
--- >vt_USERDEFINED      =     29 :: VarType
--- >vt_LPSTR            =     30 :: VarType
--- >vt_LPWSTR           =     31 :: VarType
--- >vt_RECORD           =     36 :: VarType
--- >vt_INT_PTR          =     37 :: VarType
--- >vt_UINT_PTR         =     38 :: VarType
--- >vt_FILETIME         =     64 :: VarType
--- >vt_BLOB             =     66 :: VarType
--- >vt_STREAM           =     66 :: VarType
--- >vt_STORAGE          =     67 :: VarType
--- >vt_STREAMED_OBJECT  =     68 :: VarType
--- >vt_STORED_OBJECT    =     69 :: VarType
--- >vt_BLOB_OBJECT      =     70 :: VarType
--- >vt_CF               =     71 :: VarType
--- >vt_CLSID            =     72 :: VarType
--- >vt_VERSIONED_STREAM =     73 :: VarType
--- >vt_BSTR_BLOB        =  0xfff :: VarType
--- >vt_VECTOR           = 0x1000 :: VarType
--- >vt_ARRAY            = 0x2000 :: VarType
--- >vt_BYREF            = 0x4000 :: VarType
--- >vt_RESERVED         = 0x8000 :: VarType
--- >vt_ILLEGAL          = 0xffff :: VarType
--- >vt_ILLEGALMASKED    =  0xfff :: VarType
--- >vt_TYPEMASK         =  0xfff :: VarType
 
 type VarType = Word16
 
@@ -154,6 +112,58 @@ instance HVarType Variant where
   hVarType _ = vt_VARIANT
 
 {- $varTypeDef
+ >vt_EMPTY            =      0 :: VarType
+ >vt_NULL             =      1 :: VarType
+ >vt_I2               =      2 :: VarType
+ >vt_I4               =      3 :: VarType
+ >vt_R4               =      4 :: VarType
+ >vt_R8               =      5 :: VarType
+ >vt_CY               =      6 :: VarType
+ >vt_DATE             =      7 :: VarType
+ >vt_BSTR             =      8 :: VarType
+ >vt_DISPATCH         =      9 :: VarType
+ >vt_ERROR            =     10 :: VarType
+ >vt_BOOL             =     11 :: VarType
+ >vt_VARIANT          =     12 :: VarType
+ >vt_UNKNOWN          =     13 :: VarType
+ >vt_DECIMAL          =     14 :: VarType
+ >vt_I1               =     16 :: VarType
+ >vt_UI1              =     17 :: VarType
+ >vt_UI2              =     18 :: VarType
+ >vt_UI4              =     19 :: VarType
+ >vt_I8               =     20 :: VarType
+ >vt_UI8              =     21 :: VarType
+ >vt_INT              =     22 :: VarType
+ >vt_UINT             =     23 :: VarType
+ >vt_VOID             =     24 :: VarType
+ >vt_HRESULT          =     25 :: VarType
+ >vt_PTR              =     26 :: VarType
+ >vt_SAFEARRAY        =     27 :: VarType
+ >vt_CARRAY           =     28 :: VarType
+ >vt_USERDEFINED      =     29 :: VarType
+ >vt_LPSTR            =     30 :: VarType
+ >vt_LPWSTR           =     31 :: VarType
+ >vt_RECORD           =     36 :: VarType
+ >vt_INT_PTR          =     37 :: VarType
+ >vt_UINT_PTR         =     38 :: VarType
+ >vt_FILETIME         =     64 :: VarType
+ >vt_BLOB             =     66 :: VarType
+ >vt_STREAM           =     66 :: VarType
+ >vt_STORAGE          =     67 :: VarType
+ >vt_STREAMED_OBJECT  =     68 :: VarType
+ >vt_STORED_OBJECT    =     69 :: VarType
+ >vt_BLOB_OBJECT      =     70 :: VarType
+ >vt_CF               =     71 :: VarType
+ >vt_CLSID            =     72 :: VarType
+ >vt_VERSIONED_STREAM =     73 :: VarType
+ >vt_BSTR_BLOB        =  0xfff :: VarType
+ >vt_VECTOR           = 0x1000 :: VarType
+ >vt_ARRAY            = 0x2000 :: VarType
+ >vt_BYREF            = 0x4000 :: VarType
+ >vt_RESERVED         = 0x8000 :: VarType
+ >vt_ILLEGAL          = 0xffff :: VarType
+ >vt_ILLEGALMASKED    =  0xfff :: VarType
+ >vt_TYPEMASK         =  0xfff :: VarType
 -}
 
 vt_EMPTY            =      0 :: VarType
@@ -172,7 +182,7 @@ vt_VARIANT          =     12 :: VarType
 vt_UNKNOWN          =     13 :: VarType
 vt_DECIMAL          =     14 :: VarType
 vt_I1               =     16 :: VarType
-vt_UI1              =     18 :: VarType
+vt_UI1              =     17 :: VarType
 vt_UI2              =     18 :: VarType
 vt_UI4              =     19 :: VarType
 vt_I8               =     20 :: VarType
